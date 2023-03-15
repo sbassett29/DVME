59e88b80ee3d3a815567ecf53b5951a66b3e730a
sbassett <sbassett@wikimedia.org>
2023-03-14 23:36:56 -0500

    * A few more updates to the XSS and SQLi pages

--- includes/SpecialSQLi.php
+++ includes/SpecialSQLi.php
@@ -7,23 +7,61 @@
 
 namespace MediaWiki\Extension\DVME;
 
-class SpecialSQLi extends \SpecialPage {
+class SpecialSQLi extends \FormSpecialPage {
+
+	private $status;
 
 	/**
 	 * Initialize the special page.
 	 */
 	public function __construct() {
-		// A special page should at least have a name.
-		// We do this by calling the parent class (the SpecialPage class)
-		// constructor method with the name as first and only parameter.
 		parent::__construct( 'SQLi' );
 	}
 
 	/**
-	 * Display page with XSS vulns
+	 * Display page with SQLi vulns
 	 */
-	public function execute() {
+	public function execute( $par ) {
 		$out = $this->getOutput();
 		$out->setPageTitle( $this->msg( 'dvme-sqli-title' ) );
+		$out->disallowUserJs();
+		parent::execute( $par );
+	}
+
+	protected function getDisplayFormat() {
+		return 'ooui';
+	}
+
+	public function getDescription() {
+		return $this->msg( 'dvme-sqli-title-short' )->text();
+	}
+
+	public function isListed() {
+		return true;
+	}
+
+	/**
+	 * Get form fields
+	 */
+	protected function getFormFields() {
+		$user = $this->getUser();
+		return [
+			'FavoriteDatabase' => [
+				'type' => 'text',
+				'autofocus' => true,
+				'maxlength' => 255,
+				'label-message' => 'dvme-sqli-favorite-database',
+				'default' => '',
+			]
+		];
+	}
+
+	/**
+	 * onSubmit
+	 */
+	public function onSubmit( array $data ) {
+		$out = $this->getOutput();
+		$html = "hello";
+		$out->addHTML ( $html );
 	}
 }
--- includes/SpecialXSS.php
+++ includes/SpecialXSS.php
@@ -7,23 +7,69 @@
 
 namespace MediaWiki\Extension\DVME;
 
-class SpecialXSS extends \SpecialPage {
+class SpecialXSS extends \FormSpecialPage {
+
+	private $status;
 
 	/**
 	 * Initialize the special page.
 	 */
 	public function __construct() {
-		// A special page should at least have a name.
-		// We do this by calling the parent class (the SpecialPage class)
-		// constructor method with the name as first and only parameter.
 		parent::__construct( 'XSS' );
 	}
 
 	/**
 	 * Display page with XSS vulns
 	 */
-	public function execute() {
+	public function execute( $par ) {
 		$out = $this->getOutput();
 		$out->setPageTitle( $this->msg( 'dvme-xss-title' ) );
+		$out->disallowUserJs();
+
+		$request = $this->getRequest();
+		$errorMsg = $request->getVal( 'errorMsg' );
+		$out->addHTML (
+			\Html::rawElement( 'span', [], $errorMsg )
+		);
+
+		parent::execute( $par );
+	}
+
+	public function getDescription() {
+		return $this->msg( 'dvme-xss-title-short' )->text();
+	}
+
+	protected function getDisplayFormat() {
+		return 'ooui';
+	}
+
+	public function isListed() {
+		return true;
+	}
+
+	/**
+	 * Get form fields
+	 */
+	protected function getFormFields() {
+		return [
+			'FavoriteColor' => [
+				'type' => 'text',
+				'autofocus' => true,
+				'maxlength' => 255,
+				'label-message' => 'dvme-xss-favorite-color',
+				'default' => '',
+			]
+		];
+	}
+
+	/**
+	 * onSubmit
+	 */
+	public function onSubmit( array $data ) {
+		$out = $this->getOutput();
+		$html = \Html::rawElement( 'strong', [], $this->msg( 'dvme-xss-favorite-color' ).' ' ) .
+			\Html::rawElement( 'span', [], $data['FavoriteColor'] );
+ 
+		$out->addHTML ( $html );
 	}
 }
